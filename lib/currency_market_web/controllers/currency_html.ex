21f65c00a01f93336e0872a20680f1bee999008d
Tania Sasaran <taniasasaran@gmail.com>
2023-03-31 13:52:32 +0300

    added variation computations

--- lib/currency_market_web/controllers/currency_html.ex
+++ lib/currency_market_web/controllers/currency_html.ex
@@ -17,12 +17,18 @@ defmodule CurrencyMarketWeb.CurrencyHTML do
     [_ | next] = values
     values
     |> Enum.zip(next)
-    |> Enum.map(fn {a, b} -> a - b end)
+    |> Enum.map(fn {a, b} -> Float.round(a - b, 4) end)
+    |> List.insert_at(0, 0/1)
+  end
+
+  def variation2(value1, value2) do
+    value2.record_value - value1.record_value
   end
 
   def variation_percent(currency) do
     currency
     |> variation()
-    |> Enum.map(fn value -> value/100.0 end)
+    |> Enum.map(fn value -> Float.to_string(value*100) <> "%"  end)
+    # |> Enum.map(fn value -> value)
   end
 end
